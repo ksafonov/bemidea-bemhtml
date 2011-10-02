2e65b82d8dda11cc2680a258cca46e88092e7daa
Sergey Kryzhanovsky <skryzhanovsky@ya.ru>
2011-10-02 21:02:38 +0300

    Embedded JS prefixes to avoid warnings

--- src/bem/idea/bemhtml/lang/injection/BemHtmlJavaScriptInjector.java
+++ src/bem/idea/bemhtml/lang/injection/BemHtmlJavaScriptInjector.java
@@ -10,21 +10,32 @@ import org.jetbrains.annotations.NotNull;
 
 public class BemHtmlJavaScriptInjector implements LanguageInjector {
 
+    private final static String jsPrefix =
+            "function ___BEMHTMLPLUGIN___() {" +
+            "this.block=1;" +
+            "this.elem=1;" +
+            "this.mods=1;" +
+            "this.elemMods=1;" +
+            "this.ctx=1;" +
+            "this.isFirst=1;" +
+            "this.isLast=1;" +
+            "this.position=1;";
+
     public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost _host,
                                      @NotNull InjectedLanguagePlaces registrar) {
         if (_host instanceof BemHtmlJavaScriptCode) {
             BemHtmlJavaScriptCode host = (BemHtmlJavaScriptCode)_host;
             registrar.addPlace(Language.findLanguageByID("JavaScript"),
-                            new TextRange(0, host.getTextLength()), null, null);
+                            new TextRange(0, host.getTextLength()), jsPrefix, "}");
         } else if (_host instanceof BemHtmlAsgnExpr) {
             BemHtmlAsgnExpr host = (BemHtmlAsgnExpr)_host;
             registrar.addPlace(Language.findLanguageByID("JavaScript"),
-                            new TextRange(0, host.getTextLength()), "function ___BEMHTMLPLUGIN___() {return ", "}");
+                            new TextRange(0, host.getTextLength()), jsPrefix + "return ", "}");
         } else if (_host instanceof BemHtmlBemValue) {
             BemHtmlBemValue host = (BemHtmlBemValue)_host;
             if (!host.getText().matches("^[\\w\\-]+$")) {
                 registrar.addPlace(Language.findLanguageByID("JavaScript"),
-                                new TextRange(0, host.getTextLength()), "function ___BEMHTMLPLUGIN___() {return ", "}");
+                                new TextRange(0, host.getTextLength()), jsPrefix + "return ", "}");
             }
         }
     }
